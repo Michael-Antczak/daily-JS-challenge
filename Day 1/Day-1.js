9fb14b96a5c09bd5cff0afc93d57ea2a9f6f12ec
MHDRateb <ratebalissa@gmail.com>
2017-05-22 17:34:34 +0100

    solution all trees

--- Day 1/Day-1.js
+++ Day 1/Day-1.js
@@ -1,4 +1,5 @@
 
+
 /*First tree*/
 
 var result = "";
@@ -9,6 +10,7 @@ for (var i = 0; i < 10; i++) {
 
 
 /*Second tree */
+
 var counter = 0;
 for (var i = 10; i > 0; i--) {
     counter++;
@@ -22,3 +24,33 @@ for (var i = 10; i > 0; i--) {
     console.log(result);
 }
 
+/*Third tree */
+
+var counter = 1;
+for (var i = 12; i > 0; i--) {
+    counter = counter + 2;
+    var result = "";
+    if (i > 2) {
+
+
+        for (var j = 0; j < i; j++) {
+            result += " ";
+        }
+        for (var k = counter - 2; k > 0; k--) {
+            result += "*";
+        }
+
+        console.log(result);
+    }
+    else {
+        for (var j = 11; j > 0; j--) {
+            result += " ";
+        }
+        for (var k = 5; k > 2; k--) {
+            result += "*";
+        }
+        console.log(result);
+
+    }
+
+}
