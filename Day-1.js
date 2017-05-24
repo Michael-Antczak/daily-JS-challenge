f03aa91f1c574155c8f769342502bab3add1faec
Hervin Efrain Garcia Cardona <hervi@LAPTOP-MR2VQS11.(none)>
2017-05-24 19:58:32 +0100

    add chance

---
+++ Day-1.js
@@ -0,0 +1,32 @@
+
+// exaple 
+
+var add = function (primeros, segundos) {
+    console.log('5+8 is =');
+    return primeros + segundos;
+};
+var result = add(5, 8);
+console.log(result);
+
+var subtract = function (primeros,segundos){ 
+console.log('10-8 is =');
+return primeros - segundos
+};
+var result = subtract(10, 8);
+console.log(result);
+
+var multiply = function (primeros,segundos){ 
+console.log('15*2 is =');
+return primeros * segundos
+};
+var result = multiply(15, 2);
+console.log(result);
+
+var divide = function (primeros,segundos){ 
+console.log('100/2 is =');
+return primeros / segundos
+};
+var result = divide(100, 2);
+console.log(result);
+
+
