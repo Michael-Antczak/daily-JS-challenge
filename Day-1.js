2fd888ee7c15c6975238c8139497b504cedf3092
Hervin Efrain Garcia Cardona <hervi@LAPTOP-MR2VQS11.(none)>
2017-05-25 21:36:55 +0100

    my firt step

--- Day-1.js
+++ Day-1.js
@@ -1,32 +1,6 @@
+// homework
 
-// exaple 
-
-var add = function (primeros, segundos) {
-    console.log('5+8 is =');
-    return primeros + segundos;
-};
-var result = add(5, 8);
-console.log(result);
-
-var subtract = function (primeros,segundos){ 
-console.log('10-8 is =');
-return primeros - segundos
-};
-var result = subtract(10, 8);
-console.log(result);
-
-var multiply = function (primeros,segundos){ 
-console.log('15*2 is =');
-return primeros * segundos
-};
-var result = multiply(15, 2);
-console.log(result);
-
-var divide = function (primeros,segundos){ 
-console.log('100/2 is =');
-return primeros / segundos
-};
-var result = divide(100, 2);
-console.log(result);
-
-
+for (var i = 0; i < 12; i++) {
+    var a = Array(i).join("*");
+    console.info(a);
+}
